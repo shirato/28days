eb31a8843f783fa9dbf42933f9c147872773a19c
shirato <satoshi.shirato@gmail.com>
2014-10-30 17:34:33 +0900

    Display message of the month
    Get message of the month from server.

--- assets/www/scripts/script.js
+++ assets/www/scripts/script.js
@@ -44,6 +44,7 @@ var countString = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th"
 var month = todayDate.getMonth() + 1;
 var date = todayDate.getDate();
 var uuid;
+var message = new Array(0);
 
 
 function onDeviceReady() {
@@ -105,6 +106,7 @@ $(document).on('pageshow', '#alarm', function () {
 $(document).on('pageshow', '#email', function () {
     console.log("email pageshow");
     loadMailSettings();
+    ajaxLoadMessage();
 });
 
 $(document).on('pageshow', '#settings', function () {
@@ -177,7 +179,7 @@ function saveCount() {
     var date = startDate.getDate();
     console.log(year + "/" + month + "/" + date);
 
-    settings = { "alarm": "off", "hour": 0, "minute": 0, "repeat": "21days", "tone": "default", "alert": "off", "name": "", "bname": "", "bemail": "", "reminder": "off", "sYear": year, "sMonth": month, "sDate": date }
+    settings = { "alarm": "off", "hour": 0, "minute": 0, "repeat": "21days", "tone": "default", "alert": "off", "name": "", "bname": "", "bemail": "", "reminder": "off", "sYear": year, "sMonth": month, "sDate": date, "active": "false"}
 
     localStorage.settings = JSON.stringify(settings);
 }
@@ -254,6 +256,10 @@ function getScreenHeight() {
     return screen.height;
 }
 
+function escapeHTML(html) {
+    return $('div').text(html).html();
+}
+
 //function myDatePicker() {
 //    var myNewDate = new Date();
 
@@ -269,6 +275,76 @@ function getScreenHeight() {
 //        $("#time").val(hours + ":" + minutes);
 //    });
 //}
+
+function ajaxLoadMessage() {
+    $.ajax({
+        url: 'http://searat.net/public/getmessage.php',
+        beforeSend: function (xhr) {
+            var credentials = "YXBwLXVzZXI6YXBwLXVzZXI=";
+            xhr.setRequestHeader("Authorization", "Basic " + credentials);
+            //xhr.setRequestHeader("Authorization", "Basic " + "YXBwLXVzZXI6YXBwLXVzZXI=");
+        },
+        //xhrFields: {
+        //    withCredentials: true
+        //},
+        type: 'GET',
+        dataType: 'json',
+        //username: 'app-user',
+        //password: 'app-user',
+        success: function (j_data) {
+            console.log("success to load");
+            if (j_data == null) {
+                alert("message is N/A");
+            } else {
+                message = j_data;
+                //message = JSON.parse(j_data);
+                $("#eompanel").find("h3").text("Email of the month (" + message.month + ")");
+                $("#eompanel").find("p").text(message.detail_eng);
+                //$("#eompanel").html("<h3>Email of the month (" + month + ")</h3> <p>" + detail + "</p>" +
+                //    '<a href="#" data-rel="close" data-role="button" data-icon="delete" data-inline="true">Close</a>');
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            var XMLMsg = "XMLHttpRequest: " + XMLHttpRequest.status;
+            var TextMsg = "textStatus: " + textStatus;
+            var ErrorMsg = "errorThrown: " + errorThrown.message;
+            alert(XMLMsg + "\n" + TextMsg + "\n" + ErrorMsg + "\n");
+        }
+    });
+}
+
+
+function ajaxSaveMethod() {
+    $('#save').button('disable');
+
+
+    $.ajax({
+        url: 'http://searat.net/public/savedata.php',
+        beforeSend: function (xhr) {
+            var credentials = "YXBwLXVzZXI6YXBwLXVzZXI=";
+            xhr.setRequestHeader("Authorization", "Basic " + credentials);
+            //xhr.setRequestHeader("Authorization", "Basic " + "YXBwLXVzZXI6YXBwLXVzZXI=");
+        },
+        //xhrFields: {
+        //    withCredentials: true
+        //},
+        type: 'POST',
+        //username: 'app-user',
+        //password: 'app-user',
+        data: { 'name': $('#name').val(), 'bname': $('#bname').val(), 'bemail': $('#bemail').val(), 'alert': $('alert').val(), 'uuid': $('uuid').val() },
+        success: function (d) {
+            alert("success");
+            $('#save').button('enable');
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            var XMLMsg = "XMLHttpRequest: " + XMLHttpRequest.status;
+            var TextMsg = "textStatus: " + textStatus;
+            var ErrorMsg = "errorThrown: " + errorThrown.message;
+            alert(XMLMsg + "\n" + TextMsg + "\n" + ErrorMsg + "\n");
+            $('#save').button('enable');
+        }
+    });
+}
 
 function ajaxSendMethod() {
     $('#send').button('disable');
